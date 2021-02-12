127955716f0a7b945ff35575d5a2699383a9d68a
LucasBoTang <botang@mie.utoronto.ca>
2021-02-12 15:38:59 -0500

    Add SPO+

--- loss/spoplus.py
+++ loss/spoplus.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+import torch
 from torch.autograd import Function
+import numpy as np
 from model import optModel
 
 class SPOPlusLoss(Function):
@@ -16,17 +18,43 @@ class SPOPlusLoss(Function):
     The SPO+ Loss is convex with subgradient. Thus, allows us to design
     an algorithm based on stochastic gradient descent.
     """
-    def __init__(self, model):
+    def __init__(self):
         """
         Args:
             model: instance of optModel
         """
         super().__init__()
+
+    @staticmethod
+    def forward(ctx, model, pred_cost, true_cost, true_sol, true_obj):
+        # check model
         assert isinstance(model, optModel), 'arg model is not an optModel'
-        self.m = model
+        # convert tenstor
+        cp = pred_cost.to('cpu').numpy()
+        c = true_cost.to('cpu').numpy()
+        w = true_sol.to('cpu').numpy()
+        z = true_obj.to('cpu').numpy()
+        # init loss
+        loss = []
+        wq = []
+        for i in range(len(z)):
+            assert abs(z[i] - np.dot(c[i], w[i])) < 1e-5, 'Solution does not macth the objective value.'
+            # solve
+            model.setObj(2 * cp[i] - c[i])
+            solq, objq = model.solve()
+            # calculate loss
+            loss.append(- objq + 2 * np.dot(cp[i], w[i]) - z[i])
+            # solution
+            wq.append(solq)
+        # convert to tensor
+        loss = torch.FloatTensor(loss).to(pred_cost.device)
+        wq = torch.FloatTensor(wq).to(pred_cost.device)
+        # save solutions
+        ctx.save_for_backward(true_sol, wq)
+        return loss
 
-    def forward(pred_cost, true_cost, true_sol):
-        pass
-
-    def backward(grad_output):
-        pass
+    @staticmethod
+    def backward(ctx, grad_output):
+        w, wq = ctx.saved_tensors
+        grad = 2 * (w - wq).mean(0)
+        return None, grad_output * grad, None, None, None
