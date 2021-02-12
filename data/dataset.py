85d80c7c3efab3859046b777abc781cef4830f1e
LucasBoTang <botang@mie.utoronto.ca>
2021-02-12 08:01:28 -0500

    Add dataset

---
+++ data/dataset.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import time
+import numpy as np
+import torch
+from torch.utils.data import Dataset
+from tqdm import tqdm
+from model import optModel
+
+class optDataset(Dataset):
+    """optimization problem Dataset"""
+
+    def __init__(self, model, feats, costs):
+        """
+        Args:
+            model: an instance of optModel
+        """
+        assert isinstance(model, optModel), 'arg model is not an optModel'
+        self.model = model
+        # data
+        self.x = feats
+        self.c = costs
+        # find optimal solutions
+        self.w = self.getSols()
+
+    def getSols(self):
+        """
+        get optimal solutions for all cost vectors
+        """
+        sols = []
+        print('Solve optimization problems...')
+        time.sleep(1)
+        for c in tqdm(self.c):
+            sol = self.solve(c)
+            sols.append(sol)
+        return np.array(sols)
+
+    def solve(self, cost):
+        """
+        solve optimization problem to get optimal solutions with given cost
+        """
+        self.model.setObj(cost)
+        sol = self.model.solve()
+        return sol
+
+    def __len__(self):
+        """
+        return the number of optimization problems
+        """
+        return len(self.c)
+
+    def __getitem__(self, index):
+        """
+        return feature, cost and optimal solution
+        """
+        return torch.from_numpy(self.x[index]), \
+               torch.from_numpy(self.c[index]), \
+               torch.from_numpy(self.w[index])
