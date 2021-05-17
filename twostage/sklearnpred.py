f0f557bdad19e53fc1f9bff6b280b967b145b250
LucasBoTang <botang@mie.utoronto.ca>
2021-05-17 17:44:49 -0400

    Add repr

---
+++ twostage/sklearnpred.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+class sklearnPred:
+    """two-stage prediction and optimization with scikit-learn"""
+
+    def __init__(self, pmodel, omodel):
+        print(pmodel)
+        print(omodel)
