655dac9a9f867e7da646ded56f025ee7826512f0
LucasBoTang <botang@mie.utoronto.ca>
2021-05-17 23:48:38 -0400

    Add two-stage

--- twostage/sklearnpred.py
+++ twostage/sklearnpred.py
@@ -1,9 +1,42 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+import numpy as np
+from sklearn.base import clone
+
 class sklearnPred:
     """two-stage prediction and optimization with scikit-learn"""
 
     def __init__(self, pmodel, omodel):
-        print(pmodel)
-        print(omodel)
+        """
+        Args:
+            pmodel: scikit-learn regression model
+            omodel: optModel
+        """
+        self.predictor = [clone(pmodel) for _ in range(omodel.num_cost)]
+        self.optimizer = omodel
+        self.trained = False
+
+    def fit(self, x, c):
+        """
+        fit training data
+        Args:
+            x: features
+            c: costs for objective fucntion
+        """
+        assert len(c.shape) == 2 and c.shape[-1] == self.optimizer.num_cost, 'Dimension of cost does not macth.'
+        for j in range(self.optimizer.num_cost):
+            self.predictor[j].fit(x, c[:,j])
+        self.trained = True
+
+    def predict(self, x):
+        """
+        model prediction
+        Args:
+            x: features
+        """
+        cp = np.zeros((x.shape[0],0))
+        assert self.trained, 'This two-stage sklearnPred instance is not fitted yet.'
+        for j in range(self.optimizer.num_cost):
+            cp = np.concatenate((cp, self.predictor[j].predict(x).reshape(-1,1)), axis=1)
+        return cp
