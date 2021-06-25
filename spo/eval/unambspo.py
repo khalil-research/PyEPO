d8d98242a9f3f5440f29a27fdf55428ec4c7c495
LucasBoTang <botang@mie.utoronto.ca>
2021-06-25 00:00:14 -0400

    Add tolerance

--- spo/eval/unambspo.py
+++ spo/eval/unambspo.py
@@ -4,7 +4,7 @@
 import copy
 import numpy as np
 
-def unambSPO(pmodel, omodel, dataloader):
+def unambSPO(pmodel, omodel, dataloader, tolerance=1e-4):
     """
     calculate normalized unambiguous SPO to evaluate model performence
     args:
@@ -27,12 +27,12 @@ def unambSPO(pmodel, omodel, dataloader):
         # solve
         for j in range(cp.shape[0]):
             # accumulate loss
-            loss += calUnambSPO(omodel, cp[j], c[j].to('cpu').detach().numpy(), z[j].item())
+            loss += calUnambSPO(omodel, cp[j], c[j].to('cpu').detach().numpy(), z[j].item(), tolerance)
         optsum += abs(z).sum().item()
     # normalized
     return loss / (optsum + 1e-3)
 
-def calUnambSPO(omodel, pred_cost, true_cost, true_obj):
+def calUnambSPO(omodel, pred_cost, true_cost, true_obj, tolerance=1e-4):
     """
     calculate normalized unambiguous SPO
     args:
@@ -44,12 +44,22 @@ def calUnambSPO(omodel, pred_cost, true_cost, true_obj):
     # opt sol for pred cost
     omodel.setObj(pred_cost)
     sol, objp = omodel.solve()
-    # opt for pred cost
-    wst_omodel = omodel.addConstr(pred_cost, objp)
-    # opt model to find worst case
-    wst_omodel.setObj(-true_cost)
-    # solve
-    _, obj = wst_omodel.solve()
+    try:
+        # opt for pred cost
+        wst_omodel = omodel.addConstr(pred_cost, objp)
+        # opt model to find worst case
+        wst_omodel.setObj(-true_cost)
+        # solve
+        _, obj = wst_omodel.solve()
+    except:
+        # tolerance
+        objp += tolerance
+        # opt for pred cost
+        wst_omodel = omodel.addConstr(pred_cost, objp)
+        # opt model to find worst case
+        wst_omodel.setObj(-true_cost)
+        # solve
+        _, obj = wst_omodel.solve()
     obj = -obj
     # loss
     return obj - true_obj
