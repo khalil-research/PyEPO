638fd586eec91f1479ddc22dcf9352de0a973c70
LucasBoTang <botang@mie.utoronto.ca>
2021-06-26 18:10:08 -0400

    Update

--- spo/data/tsp.py
+++ spo/data/tsp.py
@@ -29,32 +29,23 @@ def genData(num_data, num_features, num_nodes, deg=1, noise_width=0, seed=135):
     # random coordinates
     coords = np.random.uniform(0, 5, (m,2))
     # distance matrix
-    org_dist = distance.cdist(coords, coords, 'euclidean')
+    org_dist = distance.cdist(coords, coords, 'euclidean') * 3 ** deg
     # random matrix parameter B
-    B1 = np.random.binomial(1, 0.5, (m,p)) * np.random.uniform(0, 1, (m,p))
-    B2 = np.random.binomial(1, 0.5, (m*(m-1)//2,p)) * np.random.uniform(0, 1, (m*(m-1)//2,p))
+    B = np.random.binomial(1, 0.5, (m*(m-1)//2,p)) * np.random.uniform(-2, 2, (m*(m-1)//2,p))
     # feature vectors
     x = np.random.normal(0, 1, (n,p))
     # init cost
     c = np.zeros((n, m*(m-1)//2))
     for i in range(n):
-        # from feature to node
-        cost = org_dist.copy()
-        adds = ((np.dot(B1, x[i].reshape(p,1)).T / np.sqrt(p) + 3) ** deg + 1).reshape(-1)
-        for j in range(m):
-            for k in range(m):
-                if j == k:
-                    continue
-                cost[j,k] += (adds[j] + adds[k]) / 2
         # reshape
+        cost = org_dist.copy()
         l = 0
         for j in range(m):
             for k in range(j+1, m):
                 c[i,l] = cost[j,k]
                 l += 1
         # from feature to edge
-        adds = ((np.dot(B2, x[i].reshape(p,1)).T / np.sqrt(p) + 3) ** deg + 1).reshape(-1)
-        c[i] += adds
+        c[i] += ((np.dot(B, x[i].reshape(p,1)).T / np.sqrt(p) + 3) ** deg).reshape(-1)
         # noise
         noise =  np.random.uniform(1-noise_width, 1+noise_width, m*(m-1)//2)
         c[i] = c[i] * noise
