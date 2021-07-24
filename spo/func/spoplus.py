d367e5135b2823de7dc20ba41a56770e73935dc8
LucasBoTang <botang@mie.utoronto.ca>
2021-07-24 19:08:45 -0400

    New feat: Parallel

--- spo/func/spoplus.py
+++ spo/func/spoplus.py
@@ -1,10 +1,40 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+import inspect
 import torch
 from torch.autograd import Function
 import numpy as np
+from pathos.multiprocessing import ProcessingPool
+import spo
 from spo.model import optModel
+
+def solveWithObj4Par(obj, args, model_name):
+    """
+    global solve function for parallel
+    """
+    # build model
+    model = eval('spo.model.{}'.format(model_name))(**args)
+    # set obj
+    model.setObj(obj)
+    # solve
+    sol, obj = model.solve()
+    return sol, obj
+
+
+def getArgs(model):
+    """
+    get args of model
+    """
+    for mem in inspect.getmembers(model):
+        if mem[0] == '__dict__':
+            attrs = mem[1]
+            args = {}
+            for name in attrs:
+                if name in inspect.signature(model.__init__).parameters:
+                    args[name] = attrs[name]
+            return args
+
 
 class SPOPlus(Function):
     """
@@ -30,24 +60,30 @@ class SPOPlus(Function):
         c = true_cost.to('cpu').numpy()
         w = true_sol.to('cpu').numpy()
         z = true_obj.to('cpu').numpy()
-        # init loss
-        loss = []
-        wq = []
-        for i in range(len(z)):
+        # check sol
+        ins_num = len(z)
+        for i in range(ins_num):
             assert abs(z[i] - np.dot(c[i], w[i])) / (abs(z[i]) + 1e-3) < 1e-3, \
             'Solution {} does not macth the objective value {}.'.format(np.dot(c[i], w[i]), z[i][0])
-            # solve
-            model.setObj(2 * cp[i] - c[i])
-            solq, objq = model.solve()
-            # calculate loss
-            loss.append(- objq + 2 * np.dot(cp[i], w[i]) - z[i])
-            # solution
-            wq.append(solq)
+        # get class
+        model_name = type(model).__name__
+        # get args
+        args = getArgs(model)
+        # parallel computing
+        with ProcessingPool() as pool:
+            res = pool.amap(solveWithObj4Par, 2*cp-c, [args]*ins_num, [model_name]*ins_num).get()
+        # get res
+        sol = np.array(list(map(lambda x: x[0], res)))
+        obj = np.array(list(map(lambda x: x[1], res)))
+        # calculate loss
+        loss = []
+        for i in range(ins_num):
+            loss.append(- obj[i] + 2 * np.dot(cp[i], w[i]) - z[i])
         # convert to tensor
         loss = torch.FloatTensor(loss).to(device)
-        wq = torch.FloatTensor(wq).to(device)
+        sol = torch.FloatTensor(sol).to(device)
         # save solutions
-        ctx.save_for_backward(true_sol, wq)
+        ctx.save_for_backward(true_sol, sol)
         return loss
 
     @staticmethod
