03f00625de80bae3b2e405dab4b6cf0289758f48
LucasBoTang <botang@mie.utoronto.ca>
2021-08-16 08:50:50 -0400

    Add Pyomo model

--- spo/model/__init__.py
+++ spo/model/__init__.py
@@ -7,3 +7,4 @@ Optimization Model based on solvers
 from spo.model.optmodel import optModel
 from spo.model.trivialsurgery import trivialSurgeryModel
 from spo.model import grb
+from spo.model import omo
