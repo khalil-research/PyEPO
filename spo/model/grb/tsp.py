1b65f89b1eb92d2b83120f6c16c3f6d0e3c031fa
LucasBoTang <botang@mie.utoronto.ca>
2021-08-23 22:03:49 -0400

    Add Gavish–Graves formulation

--- spo/model/grb/tsp.py
+++ spo/model/grb/tsp.py
@@ -13,10 +13,10 @@ from gurobipy import GRB
 
 from spo.model.grb import optGRBModel
 
-
-class tspModel(optGRBModel):
+class tspABModel(optGRBModel):
     """
     This class is optimization model for traveling salesman problem.
+    This model is for further implementation of different formulation.
 
     Args:
         num_nodes: number of nodes
@@ -33,9 +33,61 @@ class tspModel(optGRBModel):
     def num_cost(self):
         return len(self.edges)
 
+    def copy(self):
+        """
+        A method to copy model
+
+        Returns:
+            optModel: new copied model
+        """
+        new_model = type(self)(self.num_nodes)
+        return new_model
+
+    def getTour(self, sol):
+        """
+        A method to get a tour from solution
+
+        Args:
+            sol (list): solution
+
+        Returns:
+            list: a TSP tour
+        """
+        # active edges
+        edges = defaultdict(list)
+        for i, (j, k) in enumerate(self.edges):
+            if sol[i] > 1e-2:
+                edges[j].append(k)
+                edges[k].append(j)
+        # get tour
+        visited = {0}
+        tour = [0]
+        while len(visited) < len(edges):
+            i = tour[-1]
+            for j in edges[i]:
+                if j not in visited:
+                    tour.append(j)
+                    visited.add(j)
+                    break
+        if 0 in edges[tour[-1]]:
+            tour.append(0)
+        return tour
+
+
+
+class tspDFJModel(tspABModel):
+    """
+    This class is optimization model for traveling salesman problem.
+    This model is based on Danzig–Fulkerson–Johnson (DFJ) formulation and
+    constraint generation.
+
+    Args:
+        num_nodes: number of nodes
+    """
+
     def _getModel(self):
         """
-        A method to build Gurobi model
+        A method to build Gurobi model for DFJ
         """
         # ceate a model
         m = gp.Model("tsp")
@@ -117,59 +169,6 @@ class tspModel(optGRBModel):
                 sol[i] = 1
         return sol, self._model.objVal
 
-    def getTour(self, sol):
-        """
-        A method to get a tour from solution
-
-        Args:
-            sol (list): solution
-
-        Returns:
-            list: a TSP tour
-        """
-        # active edges
-        edges = defaultdict(list)
-        for i, (j, k) in enumerate(self.edges):
-            if sol[i] > 1e-2:
-                edges[j].append(k)
-                edges[k].append(j)
-        # get tour
-        visited = {0}
-        tour = [0]
-        while len(visited) < len(edges):
-            i = tour[-1]
-            for j in edges[i]:
-                if j not in visited:
-                    tour.append(j)
-                    visited.add(j)
-                    break
-        if 0 in edges[tour[-1]]:
-            tour.append(0)
-        return tour
-
-    def copy(self):
-        """
-        A method to copy model
-
-        Returns:
-            optModel: new copied model
-        """
-        new_model = tspModel(self.num_nodes)
-        # update model
-        self._model.update()
-        # new model
-        new_model._model = self._model.copy()
-        # variables for new model
-        x = new_model._model.getVars()
-        new_model.x = {key: x[i] for i, key in enumerate(self.edges)}
-        for i, j in self.edges:
-            new_model.x[j, i] = new_model.x[i, j]
-        # activate lazy constraints
-        new_model._model._x = new_model.x
-        new_model._model._n = len(self.nodes)
-        new_model._model.Params.lazyConstraints = 1
-        return new_model
-
     def addConstr(self, coefs, rhs):
         """
         A method to add new constraint
@@ -189,4 +188,82 @@ class tspModel(optGRBModel):
         new_model._model.addConstr(
             gp.quicksum(coefs[i] * new_model.x[k]
                         for i, k in enumerate(new_model.edges)) <= rhs)
+        return new_model
+
+
+class tspGGModel(tspABModel):
+    """
+    This class is optimization model for traveling salesman problem.
+    This model is based on Gavish–Graves (GG) formulation.
+
+    Args:
+        num_nodes: number of nodes
+    """
+    def _getModel(self):
+        """
+        A method to build Gurobi model for GG
+        """
+        # ceate a model
+        m = gp.Model("tsp")
+        # turn off output
+        m.Params.outputFlag = 0
+        # varibles
+        directed_edges = self.edges + [(j, i) for (i, j) in self.edges]
+        self.x = m.addVars(directed_edges, name="x", vtype=GRB.BINARY)
+        self.y = m.addVars(directed_edges, name="y")
+        # sense
+        m.modelSense = GRB.MINIMIZE
+        # constraints
+        m.addConstrs(self.x.sum("*", j) == 1 for j in self.nodes)
+        m.addConstrs(self.x.sum(i, "*") == 1 for i in self.nodes)
+        m.addConstrs(self.y.sum(i, "*") -
+                     gp.quicksum(self.y[j,i]
+                                 for j in self.nodes[1:]
+                                 if j != i) == 1
+                     for i in self.nodes[1:])
+        m.addConstrs(self.y[i,j] <= (len(self.nodes) - 1) * self.x[i,j]
+                     for (i,j) in self.x if i != 0)
+        return m
+
+    def setObj(self, c):
+        """
+        set objective function
+        """
+        if len(c) != self.num_cost:
+            raise ValueError("Size of cost vector cannot match vars.")
+        obj = gp.quicksum(c[k] * (self.x[i,j] + self.x[j,i])
+                          for k, (i,j) in enumerate(self.edges))
+        self._model.setObjective(obj)
+
+    def solve(self):
+        """
+        solve model
+        """
+        self._model.update()
+        self._model.optimize()
+        sol = np.zeros(self.num_cost, dtype=np.uint8)
+        for k, (i,j) in enumerate(self.edges):
+            if self.x[i,j].x > 1e-2 or self.x[j,i].x > 1e-2:
+                sol[k] = 1
+        return sol, self._model.objVal
+
+    def addConstr(self, coefs, rhs):
+        """
+        A method to add new constraint
+
+        Args:
+            coefs (ndarray): coeffcients of new constraint
+            rhs (float): right-hand side of new constraint
+
+        Returns:
+            optModel: new model with the added constraint
+        """
+        if len(coefs) != self.num_cost:
+            raise ValueError("Size of coef vector cannot cost.")
+        # copy
+        new_model = self.copy()
+        # add constraint
+        new_model._model.addConstr(
+            gp.quicksum(coefs[k] * (new_model.x[i,j] + new_model.x[j,i])
+                        for k, (i,j) in enumerate(new_model.edges)) <= rhs)
         return new_model
