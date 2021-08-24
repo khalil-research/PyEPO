a4e02446a056febd4f11f59c1bd8970ba8833edb
LucasBoTang <botang@mie.utoronto.ca>
2021-08-23 22:03:49 -0400

    Add Gavish–Graves formulation

--- spo/model/grb/__init__.py
+++ spo/model/grb/__init__.py
@@ -7,4 +7,4 @@ Optimization Model based on gurobipy
 from spo.model.grb.grbmodel import optGRBModel
 from spo.model.grb.shortestpath import shortestPathModel
 from spo.model.grb.knapsack import knapsackModel, knapsackModelRel
-from spo.model.grb.tsp import tspModel
+from spo.model.grb.tsp import tspDFJModel, tspGGModel
