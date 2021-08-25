bc452980eb7496390bf2a5cfc64c6ed8753d75ec
LucasBoTang <botang@mie.utoronto.ca>
2021-08-25 17:03:39 -0400

    Add Miller-Tucker-Zemlin formulation.

--- spo/model/grb/__init__.py
+++ spo/model/grb/__init__.py
@@ -7,4 +7,4 @@ Optimization Model based on gurobipy
 from spo.model.grb.grbmodel import optGRBModel
 from spo.model.grb.shortestpath import shortestPathModel
 from spo.model.grb.knapsack import knapsackModel
-from spo.model.grb.tsp import tspDFJModel, tspGGModel
+from spo.model.grb.tsp import tspGGModel, tspDFJModel, tspMTZModel
