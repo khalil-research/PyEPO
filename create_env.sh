18df86bca0db6d4369de65d5a25c78399db7fa7c
LucasBoTang <botang@mie.utoronto.ca>
2021-09-25 18:01:55 -0400

    Bug fix

--- create_env.sh
+++ create_env.sh
@@ -6,11 +6,12 @@
 # global vars
 VENVS_DIR="PATH_TO_VENV"
 VENV_NAME="spo"
+GRB_VER="9.1.2"
 
 # load module
 echo "Load module..."
 module load python/3.7
-module load gurobi/9.1.2
+module load gurobi/$GRB_VER
 # check if the license is set
 gurobi_cl 1> /dev/null && echo Success || echo Fail
 echo ""
@@ -21,25 +22,27 @@ if [ ! -d "./PATH_TO_VENV/spo" ]; then
   # create source
   virtualenv --no-download $VENVS_DIR/$VENV_NAME
   source $VENVS_DIR/$VENV_NAME/bin/activate
+  echo ""
 
   echo "Install requirements..."
+  # install gurobipy
+  cp -r $GUROBI_HOME/ .
+  cd $GRB_VER
+  python setup.py install
+  cd ..
+  rm -r $GRB_VER
+
   # pip install
   pip install --no-index --upgrade pip
   pip install torch==1.7.0
   pip install tqdm
   pip install numpy
+  pip install pandas
   pip install Pyomo
   pip install scipy
   pip install pathos
   pip install scikit_learn
   pip install submitit
-
-  # install gurobipy
-  cp -r $GUROBI_HOME .
-  cd 9.1.2
-  python setup.py install
-  cd ..
-  rm -r 9.1.2
 
 # activate virtual env
 else
