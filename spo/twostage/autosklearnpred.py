acfaa136055fe19c904131e405cf6346c3661c47
Bo Tang <botang@mie.utoronto.ca>
2021-11-10 19:21:22 -0500

    New feat: auto-sklearn with spo score

--- spo/twostage/autosklearnpred.py
+++ spo/twostage/autosklearnpred.py
@@ -7,11 +7,23 @@ Two-stage model with Scikit-learn predictor
 import numpy as np
 from autosklearn.regression import AutoSklearnRegressor
 
+from spo.eval.metrics import makeAutoSkScorer
 
-def autoSklearnPred():
+def autoSklearnPred(omodel):
     """
     Two-stage prediction and optimization with auto-sklearn.
+
+    Args:
+        omodel (optModel): optimization model
+
+    Returns:
+        AutoSklearnRegressor: Auto-SKlearn multi-output regression model
     """
-    predictor = AutoSklearnRegressor(time_left_for_this_task=600,
-                                     per_run_time_limit=30)
-    return predictor
+    # get metrics
+    spo_scorer = spo.eval.metrics.makeAutoSkScorer(omodel)
+    # build regressor
+    regressor = AutoSklearnRegressor(time_left_for_this_task=600,
+                                     per_run_time_limit=30,
+                                     seed=135,
+                                     metric=spo_scorer)
+    return regressor
