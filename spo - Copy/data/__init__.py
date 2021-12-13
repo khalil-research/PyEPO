c1a484259eb8ee57d7d17122a376253d5320fc67
Bo Tang <botang@mie.utoronto.ca>
2021-12-13 01:19:23 -0500

    Delete

--- spo - Copy/data/__init__.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-# coding: utf-8
-"""
-Synthetic data generation
-"""
-
-from spo.data import dataset, shortestpath, knapsack, tsp
--- spo - Copy/model/omo/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# coding: utf-8
-"""
-Optimization Model based on pyomo
-"""
-
-from spo.model.omo.omomodel import optOmoModel
-from spo.model.omo.shortestpath import shortestPathModel
-from spo.model.omo.knapsack import knapsackModel, knapsackModelRel
