316f4648aa97169c543e51db844e54eeb6789e7c
Bo Tang <botang@mie.utoronto.ca>
2021-12-19 01:07:46 -0500

    Bug fix

--- run/eval.py
+++ run/eval.py
@@ -17,17 +17,17 @@ def eval(testset, res, model, config):
     print("Evaluating...")
     if config.mthd == "2s":
         # prediction
-        c_test_pred = res.predict(testset.x)
+        c_test_pred = res.predict(testset.feats)
         truespo = 0
         unambspo = 0
         for i in tqdm(range(len(testset))):
             cp_i = c_test_pred[i]
-            c_i = testset.c[i]
-            z_i = testset.z[i,0]
+            c_i = testset.costs[i]
+            z_i = testset.objs[i,0]
             truespo += spo.eval.calTrueSPO(model, cp_i, c_i, z_i)
             unambspo += spo.eval.calUnambSPO(model, cp_i, c_i, z_i)
-        truespo /= abs(testset.z.sum() + 1e-3)
-        unambspo /= abs(testset.z.sum() + 1e-3)
+        truespo /= abs(testset.objs.sum() + 1e-3)
+        unambspo /= abs(testset.objs.sum() + 1e-3)
         time.sleep(1)
     if (config.mthd == "spo") or (config.mthd == "bb"):
         testloader = DataLoader(testset, batch_size=config.batch, shuffle=False)
