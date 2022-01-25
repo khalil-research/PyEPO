fad9069116dadc3e1bb2a739729efb8862bf3946
Bo Tang <botang@mie.utoronto.ca>
2022-01-25 02:27:45 -0500

    Update: l1 & l2 norm

--- pyepo/train/trainspo.py
+++ pyepo/train/trainspo.py
@@ -66,12 +66,12 @@ def trainSPO(reg, model, optimizer, trainloader, testloader=None, logdir="./logs
                 writer.add_scalar('Train/SPO Loss', loss.item(), cnt)
             # l1 reg
             if l1_lambd:
-                l1_reg = torch.abs(cp - c).sum(dim=1).mean()
+                l1_reg = torch.abs(cp - c).mean(dim=1).mean()
                 writer.add_scalar('Train/L1 Reg', l1_reg.item(), cnt)
                 loss += l1_lambd * l1_reg
             # l2 reg
             if l2_lambd:
-                l2_reg = ((cp - c) ** 2).sum(dim=1).mean()
+                l2_reg = ((cp - c) ** 2).mean(dim=1).mean()
                 writer.add_scalar('Train/L2 Reg', l2_reg.item(), cnt)
                 loss += l2_lambd * l2_reg
             # add hook
