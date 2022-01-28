61c6d9cda6207ef857c19500f0238c13bf2709aa
LucasBoTang <botang@mie.utoronto.ca>
2022-01-28 18:28:16 -0500

    Rename param `model` as `optmodel`

--- pyepo/eval/unambspo.py
+++ pyepo/eval/unambspo.py
@@ -11,46 +11,46 @@ import numpy as np
 from pyepo import EPO
 
 
-def unambSPO(pmodel, omodel, dataloader, tolerance=1e-5):
+def unambSPO(predmodel, optmodel, dataloader, tolerance=1e-5):
     """
     A function to evaluate model performance with normalized unambiguous SPO
 
     Args:
-        pmodel (nn): neural network predictor
-        omodel (optModel): optimization model
+        predmodel (nn): neural network predictor
+        optmodel (optModel): optimization model
         dataloader (DataLoader): Torch dataloader from optDataSet
 
     Returns:
         float: unambiguous SPO loss
     """
     # evaluate
-    pmodel.eval()
+    predmodel.eval()
     loss = 0
     optsum = 0
     # load data
     for data in dataloader:
         x, c, w, z = data
         # cuda
-        if next(pmodel.parameters()).is_cuda:
+        if next(predmodel.parameters()).is_cuda:
             x, c, w, z = x.cuda(), c.cuda(), w.cuda(), z.cuda()
         # pred cost
-        cp = pmodel(x).to("cpu").detach().numpy()
+        cp = predmodel(x).to("cpu").detach().numpy()
         # solve
         for j in range(cp.shape[0]):
             # accumulate loss
-            loss += calUnambSPO(omodel, cp[j], c[j].to("cpu").detach().numpy(),
+            loss += calUnambSPO(optmodel, cp[j], c[j].to("cpu").detach().numpy(),
                                 z[j].item(), tolerance)
         optsum += abs(z).sum().item()
     # normalized
     return loss / (optsum + 1e-7)
 
 
-def calUnambSPO(omodel, pred_cost, true_cost, true_obj, tolerance=1e-5):
+def calUnambSPO(optmodel, pred_cost, true_cost, true_obj, tolerance=1e-5):
     """
     A function to calculate normalized unambiguous SPO for a batch
 
     Args:
-        omodel (optModel): optimization model
+        optmodel (optModel): optimization model
         pred_cost (torch.tensor): predicted costs
         true_cost (torch.tensor): true costs
         true_obj (torch.tensor): true optimal objective values
@@ -61,26 +61,26 @@ def calUnambSPO(omodel, pred_cost, true_cost, true_obj, tolerance=1e-5):
     # change precision
     cp = np.around(pred_cost / tolerance).astype(int)
     # opt sol for pred cost
-    omodel.setObj(cp)
-    sol, objp = omodel.solve()
+    optmodel.setObj(cp)
+    sol, objp = optmodel.solve()
     sol = np.array(sol)
     objp = np.ceil(np.dot(cp, sol.T))
     # opt for pred cost
-    if omodel.modelSense == EPO.MINIMIZE:
-        wst_omodel = omodel.addConstr(cp, objp+1e-2)
-    if omodel.modelSense == EPO.MAXIMIZE:
-        wst_omodel = omodel.addConstr(-cp, -objp+1e-2)
+    if optmodel.modelSense == EPO.MINIMIZE:
+        wst_optmodel = optmodel.addConstr(cp, objp+1e-2)
+    if optmodel.modelSense == EPO.MAXIMIZE:
+        wst_optmodel = optmodel.addConstr(-cp, -objp+1e-2)
     # opt model to find worst case
     try:
-        wst_omodel.setObj(-true_cost)
-        _, obj = wst_omodel.solve()
+        wst_optmodel.setObj(-true_cost)
+        _, obj = wst_optmodel.solve()
     except:
         tolerance *= 10
-        return calUnambSPO(omodel, pred_cost, true_cost, true_obj, tolerance=tolerance)
+        return calUnambSPO(optmodel, pred_cost, true_cost, true_obj, tolerance=tolerance)
     obj = -obj
     # loss
-    if omodel.modelSense == EPO.MINIMIZE:
+    if optmodel.modelSense == EPO.MINIMIZE:
         loss = obj - true_obj
-    if omodel.modelSense == EPO.MAXIMIZE:
+    if optmodel.modelSense == EPO.MAXIMIZE:
         loss = true_obj - obj
     return loss
