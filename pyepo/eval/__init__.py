0ef5dac8eba97188f397a6018d3e64b3fc32013e
LucasBoTang <botang@mie.utoronto.ca>
2022-01-29 23:11:17 -0500

    Rename module `eval` as `metric`

--- pyepo/eval/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# coding: utf-8
-"""
-Performance evaluation
-"""
-
-from pyepo.eval.trueregret import calRegret, regret
-from pyepo.eval.unambregret import calUnambRegret, unambRegret
-from pyepo.eval.metrics import SPOError, makeSkScorer, makeAutoSkScorer
