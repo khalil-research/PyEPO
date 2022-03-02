Bug fix: regret calculation

pyepo/metric/metrics.py
@@ -7,85 +7,93 @@ Metrics for SKlearn model
 import numpy as np
 
 from pyepo.utlis import getArgs
+from pyepo import EPO
 
 def SPOError(pred_cost, true_cost, model_type, args):
     """
     A function to calculate normalized true regret
 
     Args:
-        pred_cost (array): predicted costs
-        true_cost (array): true costs
+        pred_cost (numpy.array): predicted costs
+        true_cost (numpy.array): true costs
         model_type (ABCMeta): optModel class type
         args (dict): optModel args
 
     Returns:
-        float: true SPO losses
+        float: regret loss
     """
     pred_cost = np.array(pred_cost)
     true_cost = np.array(true_cost)
     assert pred_cost.shape == true_cost.shape, \
     "Shape of true and predicted value does not match."
     # rebuild model
-    omodel = model_type(**args)
+    optmodel = model_type(**args)
     # init sum
     regret_sum = 0
     optobj_sum = 0
-    for i in range(pred_cost.shape[0]):
+    for c, cp in zip(true_cost, pred_cost):
         # opt sol for pred cost
-        omodel.setObj(pred_cost[i])
-        sol, _ = omodel.solve()
+        optmodel.setObj(cp)
+        sol, _ = optmodel.solve()
         # obj with true cost
-        obj = np.dot(sol, true_cost[i])
+        obj = np.dot(sol, c)
         # opt obj for true cost
-        omodel.setObj(true_cost[i])
-        _, optobj = omodel.solve()
+        optmodel.setObj(c)
+        _, optobj = optmodel.solve()
         # calculate regret
-        regret = obj - optobj
+        if optmodel.modelSense == EPO.MINIMIZE:
+            regret = obj - optobj
+        if optmodel.modelSense == EPO.MAXIMIZE:
+            regret = optobj - obj
         regret_sum += regret
         optobj_sum += np.abs(optobj)
-    return regret_sum / (optobj_sum + 1e-7)
+    # normalized regret
+    norm_regret = regret_sum / (optobj_sum + 1e-7)
+    return norm_regret
 
 
-def makeSkScorer(omodel):
+def makeSkScorer(optmodel):
     """
     A function to create sklearn scorer
 
     Args:
-        omodel (optModel): optimization model
+        optmodel (optModel): optimization model
 
     Returns:
         scorer: callable object that returns a scalar score; less is better.
     """
     from sklearn.metrics import make_scorer
     # get class
-    model_type = type(omodel)
+    model_type = type(optmodel)
     # get args
-    args = getArgs(omodel)
+    args = getArgs(optmodel)
     # build score
     SPO_scorer = make_scorer(SPOError, greater_is_better=False,
                              model_type=model_type, args=args)
     return SPO_scorer
 
 
-def makeAutoSkScorer(omodel):
+def makeAutoSkScorer(optmodel):
     """
     A function to create Auto-SKlearn scorer
 
     Args:
-        omodel (optModel): optimization model
+        optmodel (optModel): optimization model
 
     Returns:
         scorer: callable object that returns a scalar score; less is better.
     """
     from autosklearn.metrics import make_scorer
     # get class
-    model_type = type(omodel)
+    model_type = type(optmodel)
     # get args
-    args = getArgs(omodel)
+    args = getArgs(optmodel)
     # build score
     SPO_scorer = make_scorer(name='SPO_error',
                              score_func=SPOError,
                              greater_is_better=False,
+                             needs_proba=False,
+                             needs_threshold=False,
                              model_type=model_type,
                              args=args)
     return SPO_scorer
@@ -109,7 +117,7 @@ def testMSE(pred_cost, true_cost, model_type, args):
     assert pred_cost.shape == true_cost.shape, \
     "Shape of true and predicted value does not match."
     # rebuild model
-    omodel = model_type(**args)
+    optmodel = model_type(**args)
     # init sum
     regret_sum = 0
     optobj_sum = 0
@@ -119,21 +127,21 @@ def testMSE(pred_cost, true_cost, model_type, args):
     return np.square(pred_cost - true_cost).mean()
 
 
-def makeTestMSEScorer(omodel):
+def makeTestMSEScorer(optmodel):
     """
     A function to create MSE scorer for testing
 
     Args:
-        omodel (optModel): optimization model
+        optmodel (optModel): optimization model
 
     Returns:
         scorer: callable object that returns a scalar score; less is better.
     """
     from autosklearn.metrics import make_scorer
     # get class
-    model_type = type(omodel)
+    model_type = type(optmodel)
     # get args
-    args = getArgs(omodel)
+    args = getArgs(optmodel)
     # build score
     mse_scorer = make_scorer(name='test_error',
                              score_func=testMSE,