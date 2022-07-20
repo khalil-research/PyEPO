ca5c21d14febea8de31be5bcb985cfcffd9fc6b8
LucasBoTang <botang@mie.utoronto.ca>
2022-07-20 12:29:17 -0400

    Update config

--- config/config_sp.py
+++ config/config_sp.py
@@ -96,7 +96,7 @@ config_spo = deepcopy(config)
 # method
 config_spo.mthd = "spo"
 # time limit
-config_spo.timeout = 2
+config_spo.timeout = 6
 
 ## training configuration
 # size of neural network hidden layers
@@ -128,7 +128,7 @@ config_dbb.mthd = "dbb"
 # loss
 config_dbb.loss = "r"
 # time limit
-config_dbb.timeout = 3
+config_dbb.timeout = 10
 
 ## training configuration
 # size of neural network hidden layers
