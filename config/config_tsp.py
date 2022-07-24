124605f90831f737f1357fbdd21397d373461425
LucasBoTang <botang@mie.utoronto.ca>
2022-07-24 13:26:09 -0400

    Update config: tsp

--- config/config_tsp.py
+++ config/config_tsp.py
@@ -96,7 +96,7 @@ config_spo = deepcopy(config)
 # method
 config_spo.mthd = "spo"
 # time limit
-config_spo.timeout = 10
+config_spo.timeout = 12
 
 ## training configuration
 # size of neural network hidden layers
@@ -128,7 +128,7 @@ config_dbb.mthd = "dbb"
 # loss
 config_dbb.loss = "r"
 # time limit
-config_dbb.timeout = 15
+config_dbb.timeout = 18
 
 ## training configuration
 # size of neural network hidden layers
