c4b2a13416044f722bf31f02c3ff19c2e06fee6e
LucasBoTang <botang@mie.utoronto.ca>
2022-09-29 12:19:57 -0400

    Delete code

--- pkg/pyepo/EPO.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-"""
-Constants
-"""
-# sense
-MINIMIZE = 1
-MAXIMIZE = -1
--- pkg/pyepo/func/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# coding: utf-8
-"""
-Pytorch autograd function for SPO training
-"""
-
-from pyepo.func.blackbox import blackboxOpt
-from pyepo.func.spoplus import SPOPlus
