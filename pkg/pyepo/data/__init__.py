5ffcb150050f8370f4220827946e4316711c1293
LucasBoTang <botang@mie.utoronto.ca>
2022-09-29 12:19:57 -0400

    Delete code

--- pkg/pyepo/data/__init__.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-# coding: utf-8
-"""
-Synthetic data generation
-"""
-
-from pyepo.data import dataset, shortestpath, knapsack, tsp
--- pkg/pyepo/model/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# coding: utf-8
-"""
-Optimization Model based on solvers
-"""
-
-from pyepo.model import opt
-from pyepo.model import grb
-from pyepo.model import omo
