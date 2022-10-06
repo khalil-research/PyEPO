177521ddb65a4773d2522db4cd55600afbb8f51e
LucasBoTang <botang@mie.utoronto.ca>
2022-10-06 14:01:29 -0400

    New feat: Perturbed Fenchel-Young loss

--- pkg/pyepo/func/perturbed.py
+++ pkg/pyepo/func/perturbed.py
@@ -87,7 +87,7 @@ class perturbedOptFunc(Function):
     def forward(ctx, pred_cost, optmodel, n_samples, epsilon,
                 processes, solve_ratio, module):
         """
-        Forward pass for DBB
+        Forward pass for perturbed
 
         Args:
             pred_cost (torch.tensor): a batch of predicted values of the cost
@@ -117,7 +117,7 @@ class perturbedOptFunc(Function):
                 module.solpool = np.concatenate((module.solpool, sols))
         else:
             ptb_sols = _cache_in_pass(ptb_c, optmodel, module.solpool)
-        # expectation
+        # solution expectation
         e_sol = ptb_sols.mean(axis=1)
         # convert to tensor
         noises = torch.FloatTensor(noises).to(device)
@@ -134,7 +134,7 @@ class perturbedOptFunc(Function):
     @staticmethod
     def backward(ctx, grad_output):
         """
-        Backward pass for DBB
+        Backward pass for perturbed
         """
         ptb_sols, noises = ctx.saved_tensors
         optmodel = ctx.optmodel
@@ -145,6 +145,136 @@ class perturbedOptFunc(Function):
                             torch.einsum("bnd,bd->bn", ptb_sols, grad_output))
         grad /= n_samples * epsilon
         return grad, None, None, None, None, None, None
+
+
+class perturbedFenchelYoung(nn.Module):
+    """
+    A autograd module for Fenchel-Young loss using perturbation techniques.The
+    use of the loss improves the algorithmic by the specific expression of the
+    gradients of the loss.
+
+    For the perturbed optimizer, the cost vector need to be predicted from
+    contextual data and are perturbed with Gaussian noise.
+
+    The Fenchel-Young loss allows to directly optimize a loss between the features
+    and solutions with less computation. Thus, allows us to design an algorithm
+    based on stochastic gradient descent.
+    """
+
+    def __init__(self, optmodel, n_samples=10, epsilon=1.0, processes=1,
+                 solve_ratio=1, dataset=None):
+        """
+        Args:
+            optmodel (optModel): an PyEPO optimization model
+            n_samples (int): number of Monte-Carlo samples
+            epsilon (float): the amplitude of the perturbation
+            processes (int): number of processors, 1 for single-core, 0 for all of cores
+            solve_ratio (float): the ratio of new solutions computed during training
+            dataset (None/optDataset): the training data
+        """
+        super().__init__()
+        # optimization model
+        if not isinstance(optmodel, optModel):
+            raise TypeError("arg model is not an optModel")
+        self.optmodel = optmodel
+        # number of samples
+        self.n_samples = n_samples
+        # perturbation amplitude
+        self.epsilon = epsilon
+        # num of processors
+        if processes not in range(mp.cpu_count()+1):
+            raise ValueError("Invalid processors number {}, only {} cores.".
+                format(processes, mp.cpu_count()))
+        self.processes = processes
+        print("Num of cores: {}".format(self.processes))
+        # solution pool
+        self.solve_ratio = solve_ratio
+        if (self.solve_ratio < 0) or (self.solve_ratio > 1):
+            raise ValueError("Invalid solving ratio {}. It should be between 0 and 1.".
+                format(self.solve_ratio))
+        self.solpool = None
+        if self.solve_ratio < 1: # init solution pool
+            if not isinstance(dataset, optDataset): # type checking
+                raise TypeError("dataset is not an optDataset")
+            self.solpool = dataset.sols.copy()
+        # build optimizer
+        self.pfy = perturbedFenchelYoungFunc()
+
+    def forward(self, pred_cost, true_sol):
+        """
+        Forward pass
+        """
+        sols = self.pfy.apply(pred_cost, true_sol, self.optmodel, self.n_samples,
+                              self.epsilon, self.processes, self.solve_ratio,
+                              self)
+        return sols
+
+
+class perturbedFenchelYoungFunc(Function):
+    """
+    A autograd function for Fenchel-Young loss using perturbation techniques.
+    """
+
+    @staticmethod
+    def forward(ctx, pred_cost, true_sol, optmodel, n_samples, epsilon,
+                processes, solve_ratio, module):
+        """
+        Forward pass for perturbed Fenchel-Young loss
+
+        Args:
+            pred_cost (torch.tensor): a batch of predicted values of the cost
+            true_sol (torch.tensor): a batch of true optimal solutions
+            optmodel (optModel): an PyEPO optimization model
+            n_samples (int): number of Monte-Carlo samples
+            epsilon (float): the amplitude of the perturbation
+            processes (int): number of processors, 1 for single-core, 0 for all of cores
+            solve_ratio (float): the ratio of new solutions computed during training
+            module (nn.Module): blackboxOpt modeul
+
+        Returns:
+            torch.tensor: solution expectations with perturbation
+        """
+        # get device
+        device = pred_cost.device
+        # convert tenstor
+        cp = pred_cost.detach().to("cpu").numpy()
+        w = true_sol.detach().to("cpu").numpy()
+        # sample perturbations
+        noises = np.random.normal(0, 1, size=(n_samples, *cp.shape))
+        ptb_c = cp + epsilon * noises
+        # solve with perturbation
+        rand_sigma = np.random.uniform()
+        if rand_sigma <= solve_ratio:
+            ptb_sols = _solve_in_forward(ptb_c, optmodel, processes)
+            if solve_ratio < 1:
+                sols = ptb_sols.reshape(-1, cp.shape[1])
+                module.solpool = np.concatenate((module.solpool, sols))
+        else:
+            ptb_sols = _cache_in_pass(ptb_c, optmodel, module.solpool)
+        # solution expectation
+        e_sol = ptb_sols.mean(axis=1)
+        # difference
+        if optmodel.modelSense == EPO.MINIMIZE:
+            diff = w - e_sol
+        if optmodel.modelSense == EPO.MAXIMIZE:
+            diff = e_sol - w
+        # loss
+        loss = np.sum(diff**2, axis=1)
+        # convert to tensor
+        diff = torch.FloatTensor(diff).to(device)
+        loss = torch.FloatTensor(loss).to(device)
+        # save solutions
+        ctx.save_for_backward(diff)
+        return loss
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        """
+        Backward pass for perturbed Fenchel-Young loss
+        """
+        grad, = ctx.saved_tensors
+        grad_output = torch.unsqueeze(grad_output, dim=-1)
+        return grad * grad_output, None, None, None, None, None, None, None
 
 
 def _solve_in_forward(ptb_c, optmodel, processes):
