3f44099433e0c73ea08667f679f484dce4f143f7
LucasBoTang <botang@mie.utoronto.ca>
2023-03-16 23:01:12 -0400

    Remove softplus

--- run/net.py
+++ run/net.py
@@ -9,15 +9,13 @@ class fcNet(nn.Module):
     """
     multi-layer fully connected neural network regression
     """
-    def __init__(self, arch, softplus=False):
+    def __init__(self, arch):
         super().__init__()
         layers = []
         for i in range(len(arch)-1):
             layers.append(nn.Linear(arch[i], arch[i+1]))
             if i < len(arch) - 2:
                 layers.append(nn.ReLU())
-        if softplus:
-            layers.append(nn.Softplus(threshold=5))
         self.main = nn.Sequential(*layers)
 
     def forward(self, x):
