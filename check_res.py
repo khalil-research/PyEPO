4f56b4b094f23547dd130affb85b2634fcc6aa1f
LucasBoTang <botang@mie.utoronto.ca>
2023-03-20 16:54:43 -0400

    Create check_res.py

---
+++ check_res.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+"""
+Check the completeness of results
+"""
+
+from pathlib import Path
+import pandas as pd
+from tqdm import tqdm
+
+# get files
+files = Path("./res").glob("**/n*[!-cv].csv")
+
+imcomp_list = []
+for f in tqdm(files):
+    res = pd.read_csv(f)
+    # check num of experiment
+    if len(res) != 10:
+        imcomp_list.append(f)
+
+# pint-out
+if len(imcomp_list) == 0:
+    print("No Incomplete Result.")
+else:
+    print("Incomplete Results:")
+    for f in imcomp_list:
+        print(f)
