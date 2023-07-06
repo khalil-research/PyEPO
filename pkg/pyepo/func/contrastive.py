b1f9ede034e9218a9dbe25bbd4072d536e61cec8
Bo Tang <botang@mie.utoronto.ca>
2023-07-06 02:15:58 -0400

    Bug fix: import

--- pkg/pyepo/func/contrastive.py
+++ pkg/pyepo/func/contrastive.py
@@ -8,6 +8,7 @@ import numpy as np
 import torch
 
 from pyepo.func.abcmodule import optModule
+from pyepo.data.dataset import optDataset
 from pyepo.func.utlis import _solveWithObj4Par, _solve_in_pass, _cache_in_pass
 
 
