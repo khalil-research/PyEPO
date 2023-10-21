02520346b0e322780c42120b015bfd329d7d247a
Bo Tang <botang@mie.utoronto.ca>
2023-10-21 00:50:28 -0400

    New method: I-MLE

--- pkg/pyepo/func/__init__.py
+++ pkg/pyepo/func/__init__.py
@@ -6,6 +6,6 @@ Pytorch autograd function for end-to-end training
 
 from pyepo.func.spoplus import SPOPlus
 from pyepo.func.blackbox import blackboxOpt, negativeIdentity
-from pyepo.func.perturbed import perturbedOpt, perturbedFenchelYoung
+from pyepo.func.perturbed import perturbedOpt, perturbedFenchelYoung, implicitMLE
 from pyepo.func.contrastive import NCE, contrastiveMAP
 from pyepo.func.rank import listwiseLTR, pairwiseLTR, pointwiseLTR
