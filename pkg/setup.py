c5c953a8d435b33a46cb230cd224c425c1f2bb57
LucasBoTang <botang@mie.utoronto.ca>
2025-03-02 03:13:43 -0500

    Add mpax model

--- pkg/setup.py
+++ pkg/setup.py
@@ -17,7 +17,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     # version
-    version = "0.4.5",
+    version = "1.0.0",
     # Github repo
     url = "https://github.com/khalil-research/PyEPO",
     # author name
