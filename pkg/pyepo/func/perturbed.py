Bug fixed: numerical stability

pkg/pyepo/func/perturbed.py
@@ -109,7 +109,7 @@ class perturbedOptFunc(Function):
         grad = torch.einsum("nbd,bn->bd",
                             noises,
                             torch.einsum("bnd,bd->bn", ptb_sols, grad_output))
-        grad /= n_samples * sigma
+        grad /= (n_samples * sigma + 1e-7)
         return grad, None
 
 
@@ -326,10 +326,10 @@ class implicitMLEFunc(Function):
             # solve with perturbation
             ptb_sols_neg = _solve_or_cache(ptb_cp_neg, module)
             # get two-side gradient
-            grad = (ptb_sols_pos - ptb_sols_neg).mean(dim=1) / (2 * module.lambd)
+            grad = (ptb_sols_pos - ptb_sols_neg).mean(dim=1) / (2 * module.lambd + 1e-7)
         else:
             # get single side gradient
-            grad = (ptb_sols_pos - ptb_sols).mean(dim=1) / module.lambd
+            grad = (ptb_sols_pos - ptb_sols).mean(dim=1) / (module.lambd + 1e-7)
         return grad, None
 
 

pkg/pyepo/func/surrogate.py
@@ -163,9 +163,9 @@ class perturbationGradient(optModule):
             obj_minus = torch.einsum("bi,bi->b", pred_cost - self.sigma * true_cost, wm)
             # loss
             if self.optmodel.modelSense == EPO.MINIMIZE:
-                loss = (obj_plus - obj_minus) / (2 * self.sigma)
+                loss = (obj_plus - obj_minus) / (2 * self.sigma + 1e-7)
             elif self.optmodel.modelSense == EPO.MAXIMIZE:
-                loss = (obj_minus - obj_plus) / (2 * self.sigma)
+                loss = (obj_minus - obj_plus) / (2 * self.sigma + 1e-7)
             else:
                 raise ValueError("Invalid modelSense. Must be EPO.MINIMIZE or EPO.MAXIMIZE.")
         # back differencing
@@ -178,9 +178,9 @@ class perturbationGradient(optModule):
             obj_minus = torch.einsum("bi,bi->b", pred_cost - self.sigma * true_cost, wm)
             # loss
             if self.optmodel.modelSense == EPO.MINIMIZE:
-                loss = (obj - obj_minus) / self.sigma
+                loss = (obj - obj_minus) / (self.sigma + 1e-7)
             elif self.optmodel.modelSense == EPO.MAXIMIZE:
-                loss = (obj_minus - obj) / self.sigma
+                loss = (obj_minus - obj) / (self.sigma + 1e-7)
             else:
                 raise ValueError("Invalid modelSense. Must be EPO.MINIMIZE or EPO.MAXIMIZE.")
         return loss