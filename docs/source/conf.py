e0451de136724014463a670d1be881c2f399e3d9
Bo Tang <botang@mit.edu>
2026-05-23 16:07:21 -0400

    Docs: Sphinx docs and pytest config polish

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,85 +1,76 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
+"""Sphinx documentation builder configuration."""
 
-# -- Path setup --------------------------------------------------------------
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
 import os
 import sys
-sys.path.append(os.path.abspath("../../pkg"))
-sys.path.append(os.path.abspath("../../pkg/pyepo/data"))
-sys.path.append(os.path.abspath("../../pkg/pyepo/model"))
-sys.path.append(os.path.abspath("../../pkg/pyepo/twostage"))
-sys.path.append(os.path.abspath("../../pkg/pyepo/func"))
-sys.path.append(os.path.abspath("../../pkg/pyepo/metric"))
+from importlib.metadata import PackageNotFoundError, version
 
-
-# -- Project information -----------------------------------------------------
+sys.path.insert(0, os.path.abspath("../../pkg"))
 
 project = "PyTorch-based End-to-End Predict-then-Optimize Tool"
 copyright = "2021, Bo Tang"
 author = "Bo Tang"
 
-# The full version, including alpha/beta/rc tags
-release = "1.1.1"
+try:
+    release = version("pyepo")
+except PackageNotFoundError:
+    release = "0.0.0+unknown"
+version = ".".join(release.split(".")[:2])
 
+# optional solver backends — mock so autoapi doesn't choke when they're absent
+autodoc_mock_imports = [
+    "coptpy",
+    "mpax",
+    "jax",
+    "jaxlib",
+    "gurobipy",
+    "pyomo",
+    "ortools",
+]
 
-# -- General configuration ---------------------------------------------------
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.todo",
+    "sphinx.ext.coverage",
+    "sphinx.ext.doctest",
+    "sphinx.ext.viewcode",
+    "autoapi.extension",
+]
 
-# Mock modules that may not be installed locally
-autodoc_mock_imports = ["coptpy", "mpax", "jax", "jaxlib", "gurobipy", "pyomo", "ortools"]
-
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named "sphinx.ext.*") or your custom
-# ones.
-extensions = ["sphinx.ext.autodoc",
-              "autoapi.extension",
-              "sphinx.ext.doctest",
-              "sphinx.ext.intersphinx",
-              "sphinx.ext.todo",
-              "sphinx.ext.coverage",
-              "sphinx.ext.mathjax",
-              "sphinx.ext.napoleon"
-              ]
-
-# Add any path that contain packages here, relative to this directory.
+# autoapi reads the package AST, so inline type annotations show up automatically
 autoapi_dirs = ["../../pkg/pyepo"]
-
-# Only document class docstring, not __init__ (avoids duplicate attribute warnings)
 autoapi_python_class_content = "class"
-
-# Suppress autoapi internal warnings
+autoapi_options = [
+    "members",
+    "undoc-members",
+    "show-inheritance",
+    "show-module-summary",
+    "imported-members",
+]
 suppress_warnings = ["autoapi.python_import_resolution"]
 
-# Add default programming language
+# napoleon: nicer rendering for Google/NumPy-style docstrings paired with PEP 484 hints
+napoleon_google_docstring = True
+napoleon_numpy_docstring = True
+napoleon_use_param = True
+napoleon_use_rtype = True
+napoleon_attr_annotations = True
+
+# cross-link external docs
+intersphinx_mapping = {
+    "python": ("https://docs.python.org/3", None),
+    "numpy": ("https://numpy.org/doc/stable/", None),
+    "torch": ("https://pytorch.org/docs/stable/", None),
+    "scipy": ("https://docs.scipy.org/doc/scipy/", None),
+    "sklearn": ("https://scikit-learn.org/stable/", None),
+}
+
 highlight_language = "python"
-
-# Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
-
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
-# icon
 html_favicon = "../../images/favicon.ico"
-
-
-# -- Options for HTML output -------------------------------------------------
-
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
 html_theme = "sphinx_rtd_theme"
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
