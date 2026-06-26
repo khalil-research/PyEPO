f29fa1c3f26792f3b67819ee1ae3ef0cc2562f69
Bo Tang <botang@mit.edu>
2026-06-26 03:31:39 -0400

    Refactor: remove Gurobi model demos

--- pkg/pyepo/model/grb/knapsack.py
+++ pkg/pyepo/model/grb/knapsack.py
@@ -5,8 +5,6 @@ Knapsack problem
 
 from __future__ import annotations
 
-import numpy as np
-
 try:
     import gurobipy as gp
     from gurobipy import GRB
@@ -81,43 +79,3 @@ class knapsackModelRel(knapsackModel):
         A forbidden method to relax MIP model
         """
         raise RuntimeError("Model has already been relaxed.")
-
-
-if __name__ == "__main__":
-    # random seed
-    np.random.seed(42)
-    # set random cost for test
-    cost = np.random.random(16)
-    weights = np.random.choice(range(300, 800), size=(2, 16)) / 100
-    capacity = [20, 20]
-
-    # solve model
-    optmodel = knapsackModel(weights=weights, capacity=capacity)  # init model
-    optmodel = optmodel.copy()
-    optmodel.setObj(cost)  # set objective function
-    sol, obj = optmodel.solve()  # solve
-    # print res
-    print(f"Obj: {obj}")
-    for i in range(16):
-        if sol[i] > 1e-3:
-            print(i)
-
-    # relax
-    optmodel = optmodel.relax()
-    optmodel.setObj(cost)  # set objective function
-    sol, obj = optmodel.solve()  # solve
-    # print res
-    print(f"Obj: {obj}")
-    for i in range(16):
-        if sol[i] > 1e-3:
-            print(i)
-
-    # add constraint
-    optmodel = optmodel.addConstr([weights[0, i] for i in range(16)], 10)
-    optmodel.setObj(cost)  # set objective function
-    sol, obj = optmodel.solve()  # solve
-    # print res
-    print(f"Obj: {obj}")
-    for i in range(16):
-        if sol[i] > 1e-3:
-            print(i)
--- pkg/pyepo/model/grb/portfolio.py
+++ pkg/pyepo/model/grb/portfolio.py
@@ -43,35 +43,3 @@ class portfolioModel(portfolioBase, optGrbModel):
         m.addConstr(x.sum() == 1, "budget")
         m.addConstr(x.T @ self.covariance @ x <= self.risk_level, "risk_limit")
         return m, x
-
-
-if __name__ == "__main__":
-    import random
-
-    from pyepo.data.portfolio import genData
-
-    # random seed
-    random.seed(42)
-    # set random cost for test
-    covariance, _, revenue = genData(num_data=100, num_features=4, num_assets=50, deg=2)
-
-    # solve model
-    optmodel = portfolioModel(num_assets=50, covariance=covariance)  # init model
-    optmodel = optmodel.copy()
-    optmodel.setObj(revenue[0])  # set objective function
-    sol, obj = optmodel.solve()  # solve
-    # print res
-    print(f"Obj: {obj}")
-    for i in range(50):
-        if sol[i] > 1e-3:
-            print(f"Asset {i}: {100 * sol[i]:.2f}%")
-
-    # add constraint
-    optmodel = optmodel.addConstr([1] * 50, 30)
-    optmodel.setObj(revenue[0])  # set objective function
-    sol, obj = optmodel.solve()  # solve
-    # print res
-    print(f"Obj: {obj}")
-    for i in range(50):
-        if sol[i] > 1e-3:
-            print(f"Asset {i}: {100 * sol[i]:.2f}%")
